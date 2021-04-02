8e24f5e3afe87526019bebb63357933fd5c5217e
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2021-04-02 17:50:40 +0100

    tests

--- use-cases/elevator-co/bootstrap/test/run.sh
+++ use-cases/elevator-co/bootstrap/test/run.sh
@@ -20,15 +20,12 @@ scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
   declare -a runScripts=(
     "$scriptDir/../bootstrap.apim-system.local.sh"
     "$scriptDir/run-test.sh"
-    "$scriptDir/get-server-logs.sh"
-    "$scriptDir/../teardown.apim-system.local.sh"
   )
 
 ############################################################################################################################
 # Run
 
   FAILED=0
-
   for runScript in ${runScripts[@]}; do
     if [ "$FAILED" -eq 0 ]; then
       echo "starting: $runScript ..."
@@ -38,6 +35,21 @@ scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
       "$runScript" > $logFile 2>&1
       code=$?; if [[ $code != 0 ]]; then echo ">>> ERROR - code=$code - runScript='$runScript' - $scriptName"; FAILED=1; fi
     fi
+  done
+
+############################################################################################################################
+# Get Container logs and stop server
+  declare -a runScripts=(
+    "$scriptDir/get-server-logs.sh"
+    "$scriptDir/../teardown.apim-system.local.sh"
+  )
+  for runScript in ${runScripts[@]}; do
+    echo "starting: $runScript ..."
+    _dirname=$(dirname "$runScript")
+    _logFile=${runScript#"$_dirname/"}
+    logFile="$LOG_DIR/$_logFile.out"; mkdir -p "$(dirname "$logFile")";
+    "$runScript" > $logFile 2>&1
+    code=$?; if [[ $code != 0 ]]; then echo ">>> ERROR - code=$code - runScript='$runScript' - $scriptName"; FAILED=1; fi
   done
 
 ##############################################################################################################################
