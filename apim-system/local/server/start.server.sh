547406443a968dc1602f9a7bcf83bcd4716b524d
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2021-04-03 09:12:16 +0100

    check for server init

--- apim-system/local/server/start.server.sh
+++ apim-system/local/server/start.server.sh
@@ -15,9 +15,6 @@ scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
 ############################################################################################################################
 # Run
 
-env | grep APIM_SYSTEM
-
-
 dockerComposeFileMac="$scriptDir/docker-compose.mac.yml"
 localMongoDBUrlMac="mongodb://host.docker.internal:$APIM_SYSTEM_MONGO_PORT/platform?retryWrites=true&w=majority"
 dockerComposeFileLinux="$scriptDir/docker-compose.linux.yml"
@@ -25,21 +22,13 @@ localMongoDBUrlLinux="mongodb://127.0.0.1:$APIM_SYSTEM_MONGO_PORT/platform?retry
 dockerComposeFile="null"
 localMongoDBUrl="null"
 platformApiServerDataVolumeMountPath="$APIM_SYSTEM_PLATFORM_API_SERVER_DATA_VOLUME_MOUNT"
+if [ ! -d "$platformApiServerDataVolumeMountPath" ]; then echo ">>> ERROR: - $scriptName - data volume not found: $platformApiServerDataVolumeMountPath"; exit 1; fi
 externalFileUserRegistry="$platformApiServerDataVolumeMountPath/$APIM_SYSTEM_PLATFORM_API_SERVER_FILE_USER_REGISTRY"
 if [ ! -f "$externalFileUserRegistry" ]; then echo ">>> ERROR: - $scriptName - user file not found: $externalFileUserRegistry"; exit 1; fi
 platformApiServerDataVolumeInternal="/platform-api-server/data"
 fileUserRegistry="$platformApiServerDataVolumeInternal/$APIM_SYSTEM_PLATFORM_API_SERVER_FILE_USER_REGISTRY"
 
 echo " >>> Starting Platform API Server in docker for $APIM_SYSTEM_USE_CASE_NAME ..."
-
-echo "hello world"
-echo "hello world"
-echo "hello world"
-echo "hello world"
-echo "hello world"
-echo "hello world"
-echo "hello world"
-
 
   uName=$(uname -s)
   case $uName in
@@ -67,11 +56,33 @@ echo "hello world"
   export APP_ID=$APIM_SYSTEM_USE_CASE_NAME
   export FILE_USER_REGISTRY=$fileUserRegistry
 
-  env | grep PLATFORM
-
   docker-compose -f "$dockerComposeFile" up -d
   if [[ $? != 0 ]]; then echo " >>> ERROR: docker compose with '$dockerComposeFile'"; exit 1; fi
   docker ps -a
+
+  echo "   >>> check docker logs if server up ..."
+    # wait until initialized
+    WORKING_DIR=$scriptDir/tmp; mkdir -p $WORKING_DIR; rm -rf $WORKING_DIR/*;
+    dockerLogsFile="$WORKING_DIR/docker.logs"
+    isInitialized=0; checks=0
+    until [[ $isInitialized -gt 2 || $checks -gt 10 ]]; do
+      ((checks++))
+      echo "   check: $checks"
+      docker logs $CONTAINER_NAME > $dockerLogsFile
+      entryListeningOnPort=$(grep -n -e "Listening on port $PLATFORM_PORT" $dockerLogsFile)
+      echo "      - entryListeningOnPort='$entryListeningOnPort'"
+      if [ ! -z "$entryListeningOnPort" ]; then ((isInitialized++)); fi
+      entryConnected2Mongo=$(grep -n -e "Connected to Mongo" $dockerLogsFile)
+      echo "      - entryConnected2Mongo='$entryConnected2Mongo'"
+      if [ ! -z "$entryConnected2Mongo" ]; then ((isInitialized++)); fi
+      entryLoadedUserRegistry=$(grep -n -e "Loaded user registry" $dockerLogsFile)
+      echo "      - entryLoadedUserRegistry='$entryLoadedUserRegistry'"
+      if [ ! -z "$entryLoadedUserRegistry" ]; then ((isInitialized++)); fi
+      if [ $isInitialized -lt 3 ]; then sleep 2s; fi
+    done
+    if [ $isInitialized -lt 3 ]; then echo " >>> ERROR: server is not initialized, checks=$checks"; exit 1; fi
+  echo "   >>> success."
+
 echo " >>> Success."
 
 ###
