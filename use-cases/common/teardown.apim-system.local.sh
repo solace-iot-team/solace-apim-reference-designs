9d43db8e5a30a67f5c93867116c2dae39b70d6a8
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2021-04-28 13:13:15 +0100

    rename uc-elevator

--- use-cases/common/teardown.apim-system.local.sh
+++ use-cases/common/teardown.apim-system.local.sh
@@ -6,7 +6,7 @@ scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
 # Environment Variables
 
   if [ -z "$APIM_BOOTSTRAP_USE_CASE_NAME" ]; then echo ">>> ERROR: - $scriptName - missing env var: APIM_BOOTSTRAP_USE_CASE_NAME"; exit 1; fi
-  if [ -z "$APIM_BOOTSTRAP_PLATFORM_API_SERVER_PORT" ]; then echo ">>> ERROR: - $scriptName - missing env var: APIM_BOOTSTRAP_PLATFORM_API_SERVER_PORT"; exit 1; fi
+  if [ -z "$APIM_BOOTSTRAP_CONNECTOR_SERVER_PORT" ]; then echo ">>> ERROR: - $scriptName - missing env var: APIM_BOOTSTRAP_CONNECTOR_SERVER_PORT"; exit 1; fi
   if [ -z "$APIM_BOOTSTRAP_DEMO_PORTAL_SERVER_PORT" ]; then echo ">>> ERROR: - $scriptName - missing env var: APIM_BOOTSTRAP_DEMO_PORTAL_SERVER_PORT"; exit 1; fi
 
 ############################################################################################################################
@@ -15,7 +15,7 @@ scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
 echo ">>> Teardown Local System for $APIM_BOOTSTRAP_USE_CASE_NAME ..."
 
   export APIM_SYSTEM_PROJECT_NAME=$APIM_BOOTSTRAP_USE_CASE_NAME
-  export APIM_SYSTEM_PLATFORM_API_SERVER_PORT=$APIM_BOOTSTRAP_PLATFORM_API_SERVER_PORT
+  export APIM_SYSTEM_CONNECTOR_SERVER_PORT=$APIM_BOOTSTRAP_CONNECTOR_SERVER_PORT
   export APIM_SYSTEM_DEMO_PORTAL_SERVER_PORT=$APIM_BOOTSTRAP_DEMO_PORTAL_SERVER_PORT
 
   runScript="$scriptDir/apim-system/local/stop.system.sh"
