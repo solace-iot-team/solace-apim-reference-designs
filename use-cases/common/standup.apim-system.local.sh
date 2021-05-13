452d62317fe321c162314e4f80c46582c584f255
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2021-05-13 08:51:17 +0100

    reworked for 0.1.0 apim connector

--- use-cases/common/standup.apim-system.local.sh
+++ use-cases/common/standup.apim-system.local.sh
@@ -5,7 +5,7 @@ scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
 ############################################################################################################################
 # Environment Variables
 
-  if [ -z "$APIM_BOOTSTRAP_USE_CASE_NAME" ]; then echo ">>> ERROR: - $scriptName - missing env var: APIM_BOOTSTRAP_USE_CASE_NAME"; exit 1; fi
+  if [ -z "$APIM_SYSTEM_PROJECT_NAME" ]; then echo ">>> ERROR: - $scriptName - missing env var: APIM_SYSTEM_PROJECT_NAME"; exit 1; fi
   if [ ! -d "$APIM_SYSTEM_CONNECTOR_SERVER_DATA_VOLUME_MOUNT" ]; then echo ">>> ERROR: - $scriptName - env var APIM_SYSTEM_CONNECTOR_SERVER_DATA_VOLUME_MOUNT, directory does not exist: '$APIM_SYSTEM_CONNECTOR_SERVER_DATA_VOLUME_MOUNT'"; exit 1; fi
   fileUserRegistry="$APIM_SYSTEM_CONNECTOR_SERVER_DATA_VOLUME_MOUNT/$APIM_SYSTEM_CONNECTOR_SERVER_FILE_USER_REGISTRY"
   if [ ! -f "$fileUserRegistry" ]; then echo ">>> ERROR: - $scriptName - env var APIM_SYSTEM_CONNECTOR_SERVER_FILE_USER_REGISTRY, file does not exist: '$fileUserRegistry'"; exit 1; fi
@@ -14,7 +14,7 @@ scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
 ############################################################################################################################
 # Run
 
-echo ">>> Standup Local System for $APIM_BOOTSTRAP_USE_CASE_NAME ..."
+echo ">>> Standup Local System for $APIM_SYSTEM_PROJECT_NAME ..."
 
   runScript="$scriptDir/apim-system/local/start.system.sh"
   $runScript
--- use-cases/common/teardown.apim-system.local.sh
+++ use-cases/common/teardown.apim-system.local.sh
@@ -5,18 +5,14 @@ scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
 ############################################################################################################################
 # Environment Variables
 
-  if [ -z "$APIM_BOOTSTRAP_USE_CASE_NAME" ]; then echo ">>> ERROR: - $scriptName - missing env var: APIM_BOOTSTRAP_USE_CASE_NAME"; exit 1; fi
-  if [ -z "$APIM_BOOTSTRAP_CONNECTOR_SERVER_PORT" ]; then echo ">>> ERROR: - $scriptName - missing env var: APIM_BOOTSTRAP_CONNECTOR_SERVER_PORT"; exit 1; fi
-  if [ -z "$APIM_BOOTSTRAP_DEMO_PORTAL_SERVER_PORT" ]; then echo ">>> ERROR: - $scriptName - missing env var: APIM_BOOTSTRAP_DEMO_PORTAL_SERVER_PORT"; exit 1; fi
+  if [ -z "$APIM_SYSTEM_PROJECT_NAME" ]; then echo ">>> ERROR: - $scriptName - missing env var: APIM_SYSTEM_PROJECT_NAME"; exit 1; fi
+  if [ -z "$APIM_SYSTEM_CONNECTOR_SERVER_PORT" ]; then echo ">>> ERROR: - $scriptName - missing env var: APIM_SYSTEM_CONNECTOR_SERVER_PORT"; exit 1; fi
+  if [ -z "$APIM_SYSTEM_DEMO_PORTAL_SERVER_PORT" ]; then echo ">>> ERROR: - $scriptName - missing env var: APIM_SYSTEM_DEMO_PORTAL_SERVER_PORT"; exit 1; fi
 
 ############################################################################################################################
 # Run
 
-echo ">>> Teardown Local System for $APIM_BOOTSTRAP_USE_CASE_NAME ..."
-
-  export APIM_SYSTEM_PROJECT_NAME=$APIM_BOOTSTRAP_USE_CASE_NAME
-  export APIM_SYSTEM_CONNECTOR_SERVER_PORT=$APIM_BOOTSTRAP_CONNECTOR_SERVER_PORT
-  export APIM_SYSTEM_DEMO_PORTAL_SERVER_PORT=$APIM_BOOTSTRAP_DEMO_PORTAL_SERVER_PORT
+echo ">>> Teardown Local System for $APIM_SYSTEM_PROJECT_NAME ..."
 
   runScript="$scriptDir/apim-system/local/stop.system.sh"
   $runScript
