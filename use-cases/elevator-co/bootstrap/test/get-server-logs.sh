4c80f153a983a5cb8f8d6499e97c3042f413043d
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2021-05-21 10:17:21 +0100

    env var

--- use-cases/elevator-co/bootstrap/test/get-server-logs.sh
+++ use-cases/elevator-co/bootstrap/test/get-server-logs.sh
@@ -5,13 +5,13 @@ scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
 ############################################################################################################################
 # Environment Variables
 
-  if [ -z "$APIM_BOOTSTRAP_USE_CASE_NAME" ]; then echo ">>> ERROR: - $scriptName - missing env var: APIM_BOOTSTRAP_USE_CASE_NAME"; exit 1; fi
+  if [ -z "$APIM_SYSTEM_PROJECT_NAME" ]; then echo ">>> ERROR: - $scriptName - missing env var: APIM_SYSTEM_PROJECT_NAME"; exit 1; fi
 
 ############################################################################################################################
 # Run
 
   echo " >>> Getting server logs ..."
-    containerName=$APIM_BOOTSTRAP_USE_CASE_NAME-apim-connector
+    containerName=$APIM_SYSTEM_PROJECT_NAME-apim-connector
     runScript="docker logs $containerName"
     $runScript
     if [[ $? != 0 ]]; then echo " >>> ERROR: $runScript "; exit 1; fi
