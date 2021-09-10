244410c1921650e2bc55293253f4c827dfcf37fc
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2021-09-10 15:47:41 +0100

    mongo version and env template cleanup

--- use-cases/car-co/bootstrap/template.source.env.sh
+++ use-cases/car-co/bootstrap/template.source.env.sh
@@ -8,6 +8,16 @@
 scriptDir=$(pwd)
 
 # USE CASE SPECIFIC BOOTSTRAP ENVIRONMENT
+# customize
+export APIM_BOOTSTRAP_SOLACE_CLOUD_TOKEN="{solace-cloud-token}"
+export APIM_BOOTSTRAP_SOLACE_CLOUD_DEV_GW_SERVICE_ID="{solace-cloud-dev-gateway-service-id}"
+export APIM_BOOTSTRAP_SOLACE_CLOUD_PROD_GW_SERVICE_ID="{solace-cloud-prod-gateway-service-id}"
+# unused:
+# export APIM_BOOTSTRAP_SOLACE_EVENT_PORTAL_API_URL="{event-portal-url}"
+# export APIM_BOOTSTRAP_SOLACE_EVENT_PORTAL_TOKEN="{solace-event-portal-token}"
+
+############################################################################################################
+# defaults
 export APIM_BOOTSTRAP_USE_CASE_NAME="uc-car-co"
 export APIM_BOOTSTRAP_ORG_NAME="car-co"
 export APIM_BOOTSTRAP_ORG_API_USER="car_co_admin"
@@ -15,11 +25,6 @@ export APIM_BOOTSTRAP_ORG_API_USER_PWD="car_co_admin_123!"
 export APIM_BOOTSTRAP_ADMIN_USER="admin"
 export APIM_BOOTSTRAP_ADMIN_USER_PWD="p3zvZFF7ka4Wrj4p"
 export APIM_BOOTSTRAP_SOLACE_CLOUD_API_URL="https://api.solace.cloud/api/v0"
-export APIM_BOOTSTRAP_SOLACE_CLOUD_TOKEN="{solace-cloud-token}"
-export APIM_BOOTSTRAP_SOLACE_EVENT_PORTAL_API_URL="{event-portal-url}"
-export APIM_BOOTSTRAP_SOLACE_EVENT_PORTAL_TOKEN="{solace-event-portal-token}"
-export APIM_BOOTSTRAP_SOLACE_CLOUD_DEV_GW_SERVICE_ID="{solace-cloud-dev-gateway-service-id}"
-export APIM_BOOTSTRAP_SOLACE_CLOUD_PROD_GW_SERVICE_ID="{solace-cloud-prod-gateway-service-id}"
 
 # Connector Server
 export APIM_BOOTSTRAP_CONNECTOR_SERVER_PROTOCOL="http"
@@ -27,14 +32,16 @@ export APIM_BOOTSTRAP_CONNECTOR_SERVER_HOST="localhost"
 export APIM_BOOTSTRAP_CONNECTOR_SERVER_PORT=9090
 export APIM_BOOTSTRAP_CONNECTOR_SERVER_DATA_VOLUME_MOUNT="$scriptDir/apim-connector-data"
 export APIM_BOOTSTRAP_CONNECTOR_SERVER_FILE_USER_REGISTRY="organization_users.json"
-export APIM_BOOTSTRAP_CONNECTOR_SERVER_DOCKER_IMAGE="solaceiotteam/apim-connector-server:latest"
+# export APIM_BOOTSTRAP_CONNECTOR_SERVER_DOCKER_IMAGE="solaceiotteam/apim-connector-server:latest"
+export APIM_BOOTSTRAP_CONNECTOR_SERVER_DOCKER_IMAGE="solaceiotteam/apim-connector-server:0.2.3"
 export APIM_BOOTSTRAP_CONNECTOR_EVENT_PORTAL_APIS_PROXY_MODE="true"
 
 # Demo Portal
 export APIM_BOOTSTRAP_DEMO_PORTAL_USER="portal_user"
 export APIM_BOOTSTRAP_DEMO_PORTAL_USER_PWD="portal_user_123!"
 export APIM_BOOTSTRAP_DEMO_PORTAL_SERVER_PORT=9091
-export APIM_BOOTSTRAP_DEMO_PORTAL_SERVER_DOCKER_IMAGE="solaceiotteam/apim-demo-portal:latest"
+# export APIM_BOOTSTRAP_DEMO_PORTAL_SERVER_DOCKER_IMAGE="solaceiotteam/apim-demo-portal:latest"
+export APIM_BOOTSTRAP_DEMO_PORTAL_SERVER_DOCKER_IMAGE="solaceiotteam/apim-demo-portal:0.1.18"
 
 env | grep APIM_BOOTSTRAP
 
